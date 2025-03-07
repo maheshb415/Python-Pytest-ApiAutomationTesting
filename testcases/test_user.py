5e3122dd3fb49635561173e83c8cf64f52192816
Mahesh Bale <mahesh.bale@stl.tech>
2025-03-07 20:58:29 +0530

    added passing and failed testcases

--- testcases/test_user.py
+++ testcases/test_user.py
@@ -1,9 +1,18 @@
 import requests
 
 
-def test_user_album_count():
+def test_user_album_count_passing():
     response = requests.get("https://jsonplaceholder.typicode.com/albums")
     resp_dict = response.json()
     print(resp_dict)
     user_count = sum([1 for entry in resp_dict if entry["userId"] == 7])
+    print('user_count=', user_count)
     assert user_count == 10
+
+def test_user_album_count_failing():
+    response = requests.get("https://jsonplaceholder.typicode.com/albums")
+    resp_dict = response.json()
+    print(resp_dict)
+    user_count = sum([1 for entry in resp_dict if entry["userId"] == 7])
+    print('user_count=', user_count)
+    assert user_count == 11
